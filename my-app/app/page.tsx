d0471e16f314b81c54877b4eba6ec81b5225bff8
Johannes Sluis <joesluis51@gmail.com>
2025-07-26 17:55:46 +0900

    testing css properties

---
+++ my-app/app/page.tsx
@@ -0,0 +1,49 @@
+"use client"
+import { useState } from "react"
+import Image from "next/image";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu"
+import { Button } from "@/components/ui/button"
+
+export default function Home() {
+  const [justifyContent, setJustifyContent] = useState("flex-start")
+  const [alignItems, setAlignItems] = useState("flex-start")
+  return (
+    <div>
+      <div className="flex gap-2 flex-col">
+        <DropdownMenu>
+        <DropdownMenuTrigger>Justify Content</DropdownMenuTrigger>
+        <DropdownMenuContent>
+          <DropdownMenuItem onClick={() => setJustifyContent("flex-start")}>flex-start</DropdownMenuItem>
+          <DropdownMenuItem onClick={() => setJustifyContent("center")}>center</DropdownMenuItem>
+          <DropdownMenuItem onClick={() => setJustifyContent("flex-end")}>flex-end</DropdownMenuItem>
+          <DropdownMenuItem onClick={() => setJustifyContent("space-between")}>space-between</DropdownMenuItem>
+          <DropdownMenuItem onClick={() => setJustifyContent("space-around")}>space-around</DropdownMenuItem>
+          <DropdownMenuItem onClick={() => setJustifyContent("space-evenly")}>space-evenly</DropdownMenuItem>
+        </DropdownMenuContent>
+        </DropdownMenu>
+        <DropdownMenu>
+          <DropdownMenuTrigger>Align Items</DropdownMenuTrigger>
+          <DropdownMenuContent>
+            <DropdownMenuItem onClick={() => setAlignItems("flex-start")}>start</DropdownMenuItem>
+            <DropdownMenuItem onClick={() => setAlignItems("flex-end")}>end</DropdownMenuItem>
+            <DropdownMenuItem onClick={() => setAlignItems("center")}>center</DropdownMenuItem>
+            <DropdownMenuItem onClick={() => setAlignItems("stretch")}>stretch</DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
+      </div>
+      <div className="flex gap-2 border-black-500 border-2 h-32" style={{ justifyContent, alignItems }}>
+        <Button>1</Button>
+        <Button>2</Button>
+        <Button>3</Button>
+        <Button>4</Button>
+      </div>
+    </div>
+  );
+}
