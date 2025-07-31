77da24cc435e7bf6366e70395a4d75f783d9398c
Johannes Sluis <joesluis51@gmail.com>
2025-07-31 16:15:29 -0700

    more customization

--- app/page.tsx
+++ app/page.tsx
@@ -15,19 +15,23 @@ export default function Home() {
   const [justifyContent, setJustifyContent] = useState("flex-start")
   const [alignItems, setAlignItems] = useState("flex-start")
   const [flexDirection, setFlexDirection] = useState<"row" | "row-reverse" | "column" | "column-reverse">("row")
+  const [gap, setGap] = useState(0)
+  const [padding, setPadding] = useState(0)
+  const [margin, setMargin] = useState(0)
+  const [buttons, setButtons] = useState(4)
   return (
     <div>
       <div className="flex gap-0 flex-row justify-end">
         <DropdownMenu>
-        <DropdownMenuTrigger className="text-white bg-black w-50 rounded-md">Justify Content</DropdownMenuTrigger>
-        <DropdownMenuContent className="text-white bg-black">
-          <DropdownMenuItem onClick={() => setJustifyContent("flex-start")}>flex-start</DropdownMenuItem>
-          <DropdownMenuItem onClick={() => setJustifyContent("center")}>center</DropdownMenuItem>
-          <DropdownMenuItem onClick={() => setJustifyContent("flex-end")}>flex-end</DropdownMenuItem>
-          <DropdownMenuItem onClick={() => setJustifyContent("space-between")}>space-between</DropdownMenuItem>
-          <DropdownMenuItem onClick={() => setJustifyContent("space-around")}>space-around</DropdownMenuItem>
-          <DropdownMenuItem onClick={() => setJustifyContent("space-evenly")}>space-evenly</DropdownMenuItem>
-        </DropdownMenuContent>
+          <DropdownMenuTrigger className="text-white bg-black w-50 rounded-md">Justify Content</DropdownMenuTrigger>
+          <DropdownMenuContent className="text-white bg-black">
+            <DropdownMenuItem onClick={() => setJustifyContent("flex-start")}>flex-start</DropdownMenuItem>
+            <DropdownMenuItem onClick={() => setJustifyContent("center")}>center</DropdownMenuItem>
+            <DropdownMenuItem onClick={() => setJustifyContent("flex-end")}>flex-end</DropdownMenuItem>
+            <DropdownMenuItem onClick={() => setJustifyContent("space-between")}>space-between</DropdownMenuItem>
+            <DropdownMenuItem onClick={() => setJustifyContent("space-around")}>space-around</DropdownMenuItem>
+            <DropdownMenuItem onClick={() => setJustifyContent("space-evenly")}>space-evenly</DropdownMenuItem>
+          </DropdownMenuContent>
         </DropdownMenu>
         <DropdownMenu>
           <DropdownMenuTrigger className="text-white bg-black w-50 rounded-md">Align Items</DropdownMenuTrigger>
@@ -47,8 +51,46 @@ export default function Home() {
             <DropdownMenuItem onClick={() => setFlexDirection("column-reverse")}>column-reverse</DropdownMenuItem>
           </DropdownMenuContent>
         </DropdownMenu>
+        <DropdownMenu>
+          <DropdownMenuTrigger className="text-white bg-black w-50 rounded-md">Gap</DropdownMenuTrigger>
+          <DropdownMenuContent className="text-white bg-black">
+            <DropdownMenuItem onClick={() => setGap(0)}>0</DropdownMenuItem>
+            <DropdownMenuItem onClick={() => setGap(10)}>10</DropdownMenuItem>
+            <DropdownMenuItem onClick={() => setGap(20)}>20</DropdownMenuItem>
+            <DropdownMenuItem onClick={() => setGap(30)}>30</DropdownMenuItem>
+            <DropdownMenuItem onClick={() => setGap(40)}>40</DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
+        <DropdownMenu>
+          <DropdownMenuTrigger className="text-white bg-black w-50 rounded-md">Padding</DropdownMenuTrigger>
+          <DropdownMenuContent className="text-white bg-black">
+            <DropdownMenuItem onClick={() => setPadding(0)}>0</DropdownMenuItem>
+            <DropdownMenuItem onClick={() => setPadding(10)}>10</DropdownMenuItem>
+            <DropdownMenuItem onClick={() => setPadding(20)}>20</DropdownMenuItem>
+            <DropdownMenuItem onClick={() => setPadding(30)}>30</DropdownMenuItem>
+            <DropdownMenuItem onClick={() => setPadding(40)}>40</DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
+        <DropdownMenu>
+          <DropdownMenuTrigger className="text-white bg-black w-50 rounded-md">Margin</DropdownMenuTrigger>
+          <DropdownMenuContent className="text-white bg-black">
+            <DropdownMenuItem onClick={() => setMargin(0)}>0</DropdownMenuItem>
+            <DropdownMenuItem onClick={() => setMargin(10)}>10</DropdownMenuItem>
+            <DropdownMenuItem onClick={() => setMargin(20)}>20</DropdownMenuItem>
+            <DropdownMenuItem onClick={() => setMargin(30)}>30</DropdownMenuItem>
+            <DropdownMenuItem onClick={() => setMargin(40)}>40</DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
+        <DropdownMenu>
+          <DropdownMenuTrigger className="text-white bg-black w-50 rounded-md">Num buttons</DropdownMenuTrigger>
+          <DropdownMenuContent className="text-white bg-black">
+            <DropdownMenuItem onClick={() => setButtons(1)}>1</DropdownMenuItem>
+            <DropdownMenuItem onClick={() => setButtons(5)}>5</DropdownMenuItem>
+            <DropdownMenuItem onClick={() => setButtons(10)}>20</DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
       </div>
-      <div className="flex gap-2 border-black-500 border-2 min-h-32" style={{ justifyContent, alignItems, flexDirection }}>
+      <div className="flex gap-2 border-black-500 border-2 min-h-32" style={{ justifyContent, alignItems, flexDirection, gap, padding, margin }}>
         <Button>1</Button>
         <Button>2</Button>
         <Button>3</Button>
